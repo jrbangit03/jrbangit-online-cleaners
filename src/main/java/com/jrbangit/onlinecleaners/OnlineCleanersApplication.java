9225b470e9279326cce1c7945546d3591bc7e550
jrbangit03 <jrbangit03@gmail.com>
2022-03-16 13:23:30 +0800

    initial commit

---
+++ src/main/java/com/jrbangit/onlinecleaners/OnlineCleanersApplication.java
@@ -0,0 +1,13 @@
+package com.jrbangit.onlinecleaners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCleanersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCleanersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrbangit/onlinecleaners/OnlineCleanersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrbangit.onlinecleaners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCleanersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
